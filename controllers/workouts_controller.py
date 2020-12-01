c89ce87f51cb81b2280354033b7dae36e8a37203
Cameron Maclean <cm.maclean@yahoo.com>
2020-12-01 20:50:54 +0000

    Added new booking GET method. Also added html for workouts/members/instructors.

--- controllers/workouts_controller.py
+++ controllers/workouts_controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint, Flask, redirect, render_template, render_template, 
 
 from models.workout import Workout
 import repositories.workout_repository as workout_repository
+import repositories.member_repository as member_repository
 
 workouts_blueprint = Blueprint("workouts", __name__)
 
@@ -14,4 +15,13 @@ def workouts():
 def show(id):
   workout = workout_repository.select(id)
   return render_template("workouts/show.html", workout = workout)
+
+@workouts_blueprint.route("/workouts/book", methods = ['GET'])
+def new_booking():
+  workouts = workout_repository.select_all()
+  members = member_repository.select_all()
+  return render_template("workouts/book.html", workouts = workouts, members = members)
+
+
+
 
